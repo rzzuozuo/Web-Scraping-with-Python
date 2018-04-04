7bffacd03a72410dd73fac1a34afed91d607837b
xiaowu <326727955@qq.com>
2018-04-05 02:41:38 +0800

    3.1 遍历wiki 域名 随机

--- web_scraping.py
+++ web_scraping.py
@@ -1,27 +1,41 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 import re #正则表达式库
+import random #随机数
+import datetime
 
 def openUrl(url):
     try:
         html = urlopen(url)
     except (HTTPError, URLError) as e:
-        return None
+        raise
+
+    return html
 
 def bsOpenUrl(url):
     try:
         html = urlopen(url)
     except (HTTPError, URLError) as e:
         return None
+
     try:
         bsObj = BeautifulSoup(html.read())
     except AttributeError as e:
         return None
     return bsObj
-bsObj = bsOpenUrl("https://en.wikipedia.org/wiki/Kevin_Bacon")
-if bsObj == None:
-    print("url could not open")
 
-for link in bsObj.find("div",{"id":"bodyContent"}).findAll("a", href = re.compile("^(/wiki/)((?!:).)*$")): # 在id=bodyContent 的div里查找满足 "/wiki/"开头 ，不含“：” 的 a
-        if 'href' in link.attrs:
-            print(link.attrs['href'])
+random.seed(datetime.datetime.now()) #使用当前时间提供随机数种子
+
+def getLinks(articleUrl):
+    bsObj = bsOpenUrl("https://en.wikipedia.org" + articleUrl)
+    if bsObj == None:
+        print("URL could not open")
+    else:
+        return bsObj.find("div",{"id":"bodyContent"}).findAll("a", href = re.compile("^(/wiki/)((?!:).)*$")) # 在id=bodyContent 的div里查找满足 "/wiki/"开头 ，不含“：” 的 a
+
+links = getLinks("/wiki/Kevin_Bacon")
+
+while len(links) > 0:
+    newArticle = links[random.randint(0, len(links) -1)].attrs["href"]
+    print(newArticle)
+    links = getLinks(newArticle)
